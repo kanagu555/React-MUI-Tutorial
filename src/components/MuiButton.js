e6fd6de7926326e4a9c9360fc5539fdaf94d030b
kanagu555 <kanagarajwhb@gmail.com>
2022-04-18 15:12:26 +0530

    Used button & Groupbutton components

--- src/components/MuiButton.js
+++ src/components/MuiButton.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Stack, Button } from '@mui/material';
+import { Stack, Button, ButtonGroup } from '@mui/material';
 import PhoneEnabledIcon from '@mui/icons-material/PhoneEnabled';
 import PhoneInTalkIcon from '@mui/icons-material/PhoneInTalk';
 
@@ -8,31 +8,59 @@ const MuiButton = () => {
     <Stack spacing={4}>
       <Stack spacing={2} direction="row">
         <Button variant="text">Text</Button>
-        <Button variant="contained" >Contained</Button>
+        <Button variant="contained">Contained</Button>
         <Button variant="outlined">Outlined</Button>
       </Stack>
-      <Stack spacing={2} direction='row'>
-        <Button variant="contained" color="primary">Primary</Button>
-        <Button variant="contained" color='secondary'>Secondary</Button>
-        <Button variant="contained" color='error'>Error</Button>
-        <Button variant="contained" color='warning'>Warning</Button>
-        <Button variant="contained" color='info'>info</Button>
-        <Button variant="contained" color='success'>Success</Button>
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+        <Button variant="contained" color="secondary">
+          Secondary
+        </Button>
+        <Button variant="contained" color="error">
+          Error
+        </Button>
+        <Button variant="contained" color="warning">
+          Warning
+        </Button>
+        <Button variant="contained" color="info">
+          info
+        </Button>
+        <Button variant="contained" color="success">
+          Success
+        </Button>
       </Stack>
-      <Stack display='block' spacing={2} direction='row'>
-        <Button variant='contained' size='small'>Small</Button>
-        <Button variant='contained' size='medium'>Medium</Button>
-        <Button variant='contained' size='large'>LArge</Button>
+      <Stack display="block" spacing={2} direction="row">
+        <Button variant="contained" size="small">
+          Small
+        </Button>
+        <Button variant="contained" size="medium">
+          Medium
+        </Button>
+        <Button variant="contained" size="large">
+          LArge
+        </Button>
       </Stack>
-      <Stack spacing={2} direction='row'>
-        <Button variant='contained' startIcon={<PhoneEnabledIcon/>} disableElevation disableRipple>call</Button>
-        <Button variant='contained' endIcon={<PhoneInTalkIcon/>}>call</Button>
+      <Stack spacing={2} direction="row">
+        <Button
+          variant="contained"
+          startIcon={<PhoneEnabledIcon />}
+          disableElevation
+          disableRipple
+        >
+          call
+        </Button>
+        <Button variant="contained" endIcon={<PhoneInTalkIcon />}>
+          call
+        </Button>
       </Stack>
-      <Stack spacing={2} direction='row'>
-
-        <Button variant='contained'>Left</Button>
-        <Button variant='contained'>Center</Button>
-        <Button variant='contained'>Right</Button>
+      <Stack direction="row">
+        <ButtonGroup variant="contained" orientation="vertical" color="success" aria-label='alignment group button'>
+          <Button>Left</Button>
+          <Button>Center</Button>
+          <Button>Right</Button>
+        </ButtonGroup>
       </Stack>
     </Stack>
   );

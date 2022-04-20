5894a53a4ebcf25dcdda5bbb8684e95927435ed0
kanagu555 <kanagarajwhb@gmail.com>
2022-04-20 14:34:07 +0530

    Mui Radio Button -1

---
+++ src/components/MuiRadioButton.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  FormControlLabel,
+  RadioGroup,
+  Radio,
+} from '@mui/material';
+
+const MuiRadioButton = () => {
+  return (
+  <Box>
+      <FormControl>
+        
+      </FormControl>
+  </Box>
+  );
+};
+
+export default MuiRadioButton;

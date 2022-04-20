b4957f570b89fe62cdb7b1587e14f07a00e2fb85
kanagu555 <kanagarajwhb@gmail.com>
2022-04-20 16:30:31 +0530

    Mui Radio Button -2

--- src/components/MuiRadioButton.js
+++ src/components/MuiRadioButton.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   Box,
   FormControl,
@@ -9,10 +9,25 @@ import {
 } from '@mui/material';
 
 const MuiRadioButton = () => {
+
+  const [value, setValue] = useState('')
+  console.log({value})
+
+  const handleChange = (event) => {
+    setValue(event.target.value)
+  }
+
   return (
   <Box>
       <FormControl>
-        
+          <FormLabel>
+            Years of Experience
+          </FormLabel>
+          <RadioGroup value={value} onChange={handleChange} row>
+              <FormControlLabel control={<Radio color='secondary' />} label='0-2' value='0-2'/>
+              <FormControlLabel control={<Radio />} label='3-5' value='3-5'/>
+              <FormControlLabel control={<Radio />} label='6-10' value='6-10'/>
+          </RadioGroup>
       </FormControl>
   </Box>
   );
